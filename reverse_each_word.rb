dda82b92daf7568a9f6b96783274f20b51e23b48
Dhara Patel <1991dharapatel@gmail.com>
2019-03-20 02:59:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,12 @@ end
 
 # reverse_each_word
 
+def reverse_each_word(sentence1)
+  new_array =[]
+  array=sentence1.split(" ")
+  for i in 0...array.length
+    array[i].reverse 
+    new_array.push(array[i].reverse)
+  end
+  
+end
